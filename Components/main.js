4015c28d3c8151290dbaad29d7c5cae538fbd82e
Gustavo <gustavojaviernunezdiaz@gmail.com>
2026-04-15 21:50:44 -0300

    Fix component import case and add useEffect for fetching todos in Main component

--- Components/main.js
+++ Components/main.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import {StyleSheet, View, Text, Pressable } from 'react-native'
 import todosServices from '../Axios/todosServices'
+import { useEffect } from 'react';
 
 
+
+
+
+useEffect(() => {
+
+    const handlePress = async () => {
+        try {
+            const data = await todosServices.getAllTodos();
+            console.log(data);
+        } catch (error) {
+            console.error("Error al obtener los todos:", error);
+        }
+    };
+
+    
+    handlePress();
+
+    
+}, []); 
+
+/*en vez de usar esto tenemos que usar el useEffect
 const handlePress = async() => {
     const data = await todosServices.getAllTodos();
     console.log(data)
 }
+    */
 
 const main = () => {
   return (
